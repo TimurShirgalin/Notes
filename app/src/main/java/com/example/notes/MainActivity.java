4686220c33da2b1594e0e7ba0442fe13014e8a36
Timur <shirgalin@hotmail.com>
2021-01-24 12:29:08 +0300

    Task1

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.notes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    Notes notes = new Notes();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setNotesData();
+    }
+
+    private void setNotesData() {
+        notes.setId(1);
+        notes.setId(2);
+        notes.setId(3);
+        notes.setDates("04.01.2021 15:19");
+        notes.setDates("12.01.2021 21:56");
+        notes.setDates("24.01.2021 12:08");
+        notes.setNotes("Однажды, в студеную зимнюю пору");
+        notes.setNotes("Я из лесу вышел; был сильный мороз");
+        notes.setNotes("Гляжу, поднимается медленно в гору");
+        notes.setNoteNames("Заметка 1");
+        notes.setNoteNames("Заметка 2");
+        notes.setNoteNames("Заметка 3");
     }
 }
---
+++ app/src/main/java/com/example/notes/Notes.java
@@ -0,0 +1,41 @@
+package com.example.notes;
+
+import java.util.ArrayList;
+
+public class Notes {
+    private ArrayList notes = new ArrayList();
+    private ArrayList dates = new ArrayList();
+    private ArrayList noteNames = new ArrayList();
+    private ArrayList id = new ArrayList();
+
+    public Notes() {
+    }
+
+    public void getNotes(int id) {
+        notes.get(id);
+    }
+
+    public void setNotes(String note) {
+        notes.add(note);
+    }
+
+    public void getDates(int id) {
+        dates.get(id);
+    }
+
+    public void setDates(String date) {
+        dates.add(date);
+    }
+
+    public void getNoteNames(int id) {
+        noteNames.get(id);
+    }
+
+    public void setNoteNames(String noteName) {
+        noteNames.add(noteName);
+    }
+
+    public void setId(int i) {
+        id.add(i);
+    }
+}
